986f1ebb5124060188da8f224b85cb089851dbbd
bo198214 <bo198214@nothing.(none)>
2009-11-15 22:41:02 +0100

    introduced class Region which works pretty good

--- tetration.py
+++ tetration.py
@@ -22,14 +22,15 @@ def expit(b,t,z,prec=100,n=10,r=None):
   lna = mpc2C(- mpmath.lambertw( - lnb))
   a = exp(lna)
   # print 'a',a,'|lna|',abs(lna)
-  a2 = mpc2C( mpmath.exp(-mpmath.lambertw(-lnb,-1)))
-  # print 'a',a2
   if r == None:
-    #r = abs(a2-a)
 
     r = 1 - abs(lna)
-    #print 'lna',lna,'r',r,'dist',abs(lna-log(z))
-    assert r>0
+
+    if not r>0: 
+      print 'lna',lna,'r',r,'dist','z',z,'|lna-lnz|',abs(lna-log(z))
+      a2 = mpc2C( mpmath.exp(-mpmath.lambertw(-lnb,-1)))
+      # print 'a',a2
+      r = abs(a2-a)
     
   zn = z
   n=0
@@ -54,67 +55,91 @@ def expit(b,t,z,prec=100,n=10,r=None):
 
 def d(b,**kwargs): 
   f = lambda z: expit(b,0.5,z,**kwargs)
-  return f(f(1.0))-b
+  res = abs(f(f(1.0))-b)
+  if res > 10**(-5):
+    print 'b',b,'d(b)',res
+  return res
 
 def tet(b,t,**kwargs):
   return expit(b,t,1,**kwargs)
-
-circ = lambda t: exp(CC(0,pi*float(t)))
-#Shell Thron boundary
-st = lambda z: exp(z*exp(-z))
-stb = lambda t: st(circ(t))
-stbtymax = fmin(lambda t: -stb(t).imag(), 0.5)[0] 
-stbtxmax = fmin(lambda t: -stb(t).real(), 0.1)[0] 
-stbtxmin = fmin(lambda t: stb(t).real(), 0.6)[0]
-
 
 def shrinked_circ(t):
   r = 0.85
   return r*exp(CC(0,2*pi*(float(t)-0.25)))+CC(r,r)
 
-def shrinked_stb(factor):
-  return lambda t: (1-factor)*stb(t)+factor*shrinked_circ(t)
+class Region:
+  def __init__(self,factor=0):
+    self.factor = factor
+    circ = lambda t: exp(CC(0,pi*float(t)))
+    #Shell Thron boundary
+    st = lambda z: exp(z*exp(-z))
+    self.stb0 = lambda t: st(circ(t))
+    self.stbtymax = fmin(lambda t: -self.stb(t).imag(), 0.5)[0] 
+    self.stbtxmax = fmin(lambda t: -self.stb(t).real(), 0.1)[0] 
+    self.stbtxmin = fmin(lambda t: self.stb(t).real(), 0.6)[0]
 
-def yints(x,eps=0):
-  stb = shrinked_stb(eps)
-  xmax = stb(stbtxmax).real()
-  xmin = stb(stbtxmin).real()
-  eta0 = exp(-exp(1.0))
-  eta1 = exp(1/exp(1.0))
-  if x <= xmin or x>=xmax:
-    return []
-  b = stb(brenth(lambda t: stb(t).real() - x, stbtxmax,stbtxmin)).imag()
-  if eta0 <= x and x <= eta1:
-    a = 0
-  else:
-    if eta1 < x:
-      a = stb(brenth(lambda t: stb(t).real() - x, 0,stbtxmax)).imag()
-    if x < eta0:
-      a = stb(brenth(lambda t: stb(t).real() - x, stbtxmin,1)).imag()
-  return [(a,b)]
+  def stb(self,t):
+    factor = self.factor
+    stb0 = self.stb0
+    
+    ret = (1-factor)*stb0(t)+factor*shrinked_circ(t)
+    return ret
   
-def xints(y,eps=0):
-  stb = shrinked_stb(eps)
-  ymax = stb(stbtymax).imag()
-  if y <= 0 or y>=ymax:
-    return []
-  a = stb(brenth(lambda t: stb(t).imag() - y, stbtymax,1)).real()
-  b = stb(brenth(lambda t: stb(t).imag() - y, 0,stbtymax)).real()
-  return [(a,b)]
+  def yints(self,x):
+    stb = self.stb
+    stbtymax = self.stbtymax
+    stbtxmax = self.stbtxmax
+    stbtxmin = self.stbtxmin
 
-def region(eps=0,xmin=None,xmax=None,ymin=None,ymax=None):
-  if xmin == None:
-    xmin = stb(stbtxmin).real()
-  if xmax == None:
     xmax = stb(stbtxmax).real()
-  xrange=(xmin,xmax)
-
-  if ymin == None:
-    ymin = 0
-  if ymax == None:
+    xmin = stb(stbtxmin).real()
+    eta0 = stb(1)
+    eta1 = stb(0)
+    if x <= xmin or x>=xmax:
+      return []
+    b = stb(brenth(lambda t: stb(t).real() - x, stbtxmax,stbtxmin)).imag()
+    if eta0 <= x and x <= eta1:
+      a = 0
+    else:
+      if eta1 < x:
+        a = stb(brenth(lambda t: stb(t).real() - x, 0,stbtxmax)).imag()
+      if x < eta0:
+        a = stb(brenth(lambda t: stb(t).real() - x, stbtxmin,1)).imag()
+    return [(a,b)]
+    
+  def xints(self,y):
+    stb = self.stb
+    stbtymax = self.stbtymax
+    stbtxmax = self.stbtxmax
+    stbtxmin = self.stbtxmin
+    
     ymax = stb(stbtymax).imag()
-  yrange=(ymin,ymax)
+    if y <= 0 or y>=ymax:
+      return []
+    a = stb(brenth(lambda t: stb(t).imag() - y, stbtymax,1)).real()
+    b = stb(brenth(lambda t: stb(t).imag() - y, 0,stbtymax)).real()
+    return [(a,b)]
+  
+  def region(self,xmin=None,xmax=None,ymin=None,ymax=None):
+    stb = self.stb
+    stbtymax = self.stbtymax
+    stbtxmax = self.stbtxmax
+    stbtxmin = self.stbtxmin
+    xints = self.xints
+    yints = self.yints
 
-  return (xrange,yrange,lambda x: yints(x,eps), lambda y: xints(y,eps))
+    if xmin == None:
+      xmin = stb(stbtxmin).real()
+    if xmax == None:
+      xmax = stb(stbtxmax).real()
+    xrange=(xmin,xmax)
+  
+    if ymin == None:
+      ymin = 0
+    if ymax == None:
+      ymax = stb(stbtymax).imag()
+    yrange=(ymin,ymax)
+  
+    return (xrange,yrange,yints,xints)
   
 # conformal_region_plot(lambda z: z, region(0.1,xmin=0.9)) + complex_parametric_plot(stb)
